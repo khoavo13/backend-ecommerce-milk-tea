336941ddf3f11bb51f25f4bb979aa21dd5988bc5
khoavo13 <vandangkhoa.vo@gmail.com>
2024-09-18 15:08:17 +0700

    create relationship entity

--- src/main/java/com/example/backend_ecommerce_milk_tea/models/Users.java
+++ src/main/java/com/example/backend_ecommerce_milk_tea/models/Users.java
@@ -1,6 +1,9 @@
 package com.example.backend_ecommerce_milk_tea.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,10 +20,19 @@ public class Users extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY) // khởi tạo giá trị id tự động bắt đầu từ 1
     private Long userId;
 
+    @NotBlank(message = "Username không được để trống")
+    @Size(min = 2, max=50, message = "Username phải có từ 2 đến 50 ký tự")
     private String username;
 
+    @NotBlank(message = "Password không được để trống")
     private String passwordHash;
+
+    @NotBlank(message = "Email không được để trống")
     private String email;
+
+    @NotBlank(message = "Số điện thoại không được để trống")
     private String phone;
+
+    @NotEmpty(message = "Role không được để trống")
     private String role;
 }
