a3bbdc9178f8785f4aa78de670d4ec76fbf78e67
dinhtien98 <ngodinhtien98@gmail.com>
2024-09-18 17:08:11 +0700

    update service

---
+++ src/main/java/com/example/backend_ecommerce_milk_tea/dtos/UserDTO.java
@@ -0,0 +1,29 @@
+package com.example.backend_ecommerce_milk_tea.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    @NotBlank(message = "Username không được để trống")
+    @Size(min = 2, max=50, message = "Username phải có từ 2 đến 50 ký tự")
+    private String username;
+
+    @NotBlank(message = "Password không được để trống")
+    private String passwordHash;
+
+    @NotBlank(message = "Email không được để trống")
+    private String email;
+
+    @NotBlank(message = "Số điện thoại không được để trống")
+    private String phone;
+
+    @NotEmpty(message = "Role không được để trống")
+    private String role;
+}
--- src/main/java/com/example/backend_ecommerce_milk_tea/services/IUserService.java
+++ src/main/java/com/example/backend_ecommerce_milk_tea/services/IUserService.java
@@ -1,4 +1,15 @@
 package com.example.backend_ecommerce_milk_tea.services;
 
+import com.example.backend_ecommerce_milk_tea.dtos.UserDTO;
+import com.example.backend_ecommerce_milk_tea.models.Users;
+
+import java.util.List;
+
 public interface IUserService {
+    Users getUserById(Long id);
+    List<Users> getAllUsers();
+    Users addUser(UserDTO userDTO);
+    Users updateUser(Long id,UserDTO userDTO);
+    void deleteUser(Long id);
+    List<Users> searchUsers(String gmail);
 }
