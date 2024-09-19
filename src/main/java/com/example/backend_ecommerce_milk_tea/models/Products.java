ee34a1105086fb6cd2e8d19f3ac3b8a88120af24
khoavo13 <vandangkhoa.vo@gmail.com>
2024-09-19 09:13:03 +0700

    config project

--- src/main/java/com/example/backend_ecommerce_milk_tea/models/Products.java
+++ src/main/java/com/example/backend_ecommerce_milk_tea/models/Products.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class Products extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // khởi tạo giá trị id tự động bắt đầu từ 1
-    private Long productId; //id sản phẩm
+    private Long id; //id sản phẩm
 
     @ManyToOne
     @JoinColumn(name="category_id")
@@ -25,10 +25,10 @@ public class Products extends BaseEntity{
 
     @NotBlank(message = "Tên sản phẩm không được để trống")
     @Size(min=5,max=50,message = "tên phải có từ 5 đến 50 ký tự")
-    private String productName; //tên sản phẩm
+    private String productName; // tên sản phẩm
 
     @NotBlank(message = "mô tả không được để trống")
-    private String productDescription; //mô tả sản phẩm
+    private String productDescription; // mô tả sản phẩm
 
-    private Double productPrice; //giá của sản phẩm
+    private Double productPrice; // giá của sản phẩm
 }
