28e2d957c1583f73bd9415a773fe8b7d8df625af
dinhtien98 <ngodinhtien98@gmail.com>
2024-09-20 11:32:50 +0700

    delete exceptions

--- src/main/java/com/example/backend_ecommerce_milk_tea/excepations/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend_ecommerce_milk_tea.excepations;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
