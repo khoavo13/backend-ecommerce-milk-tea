3d064b21f7f6a98a48190969367723f1622691ae
NhanPhan <nhanmg32@gmail.com>
2024-09-20 15:28:09 +0700

    cart feature

--- src/main/java/com/example/backend_ecommerce_milk_tea/configs/SecurityConfig.java
+++ src/main/java/com/example/backend_ecommerce_milk_tea/configs/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET,"/api/category/**").permitAll()
                 .requestMatchers(HttpMethod.POST,"/api/user/register").permitAll()
                 .requestMatchers(HttpMethod.POST,"/api/category").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET,"/api/cart/**").hasRole("USER")
 
                 .anyRequest().authenticated();
                 return http.build();
